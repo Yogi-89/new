05a6abe4c5fb921ca299537fe3cf9d3df75eaaef
muhammad Tausi'ul Arzaaq <arzararzaq72@gmail.com>
2023-08-07 15:32:28 +0700

    merapihkan file dan menambahkan design logi

--- script.js
+++
@@ -1,6 +0,0 @@
-const hideButton = document.getElementById('hideButton');
-const hiddenButton = document.getElementById('hiddenButton');
-
-hideButton.addEventListener('click', () => {
-  hiddenButton.classList.toggle('hidden');
-});
